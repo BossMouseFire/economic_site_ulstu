928239bf479099bb897716c5140a8f9c62c7a8a1
Данил <abanin-02@mail.ru>
2021-03-31 00:43:43 +0400

    Added technicalSide (3 part)

--- src/sectionsBlock/sections/technicalSide/technicalSide.js
+++ src/sectionsBlock/sections/technicalSide/technicalSide.js
@@ -5,62 +5,143 @@ export default function TechnicalSide() {
   const [positonTransactions, setPositionTransactions] = useState(0);
   const [positionCard, setPositionCard] = useState(0);
   const [positionCardBlock, setPositionCardBlock] = useState(0);
+  const [positionGroupCardBlock, setPositionGroupCardBlock] = useState(0);
+  const [positionMainerBlock, setPositionMainerBlock] = useState(0);
+  const [positionWallet, setPositionWallet] = useState(0);
+  const [positionLock, setPositionLock] = useState(0);
   const listener = (e) => {
-    let coin = document.querySelector("#mainBitcoin");
-    if (
-      window.scrollY > positionCoin &&
-      window.scrollY < positonTransactions * 0.984
-    ) {
-      coin.style.transform = `translate(0, ${
-        (window.pageYOffset - positionCoin) * 1.3
-      }px) scale(${window.scrollY <= positionCoin * 1.005 ? 1 : 0.34})`; //0.35
-      coin.style.opacity = `${window.scrollY <= positionCoin * 1.005 ? 1 : 0.5}`
-    }
+    if (window.screen.width > 768) {
+      let coin = document.querySelector("#mainBitcoin");
+      if (
+        window.scrollY > positionCoin &&
+        window.scrollY < positionWallet * 0.96
+      ) {
+        coin.style.transform = `translate(0, ${
+          (window.pageYOffset - positionCoin) * 1.1
+        }px) scale(${window.scrollY <= positionCoin * 1.005 ? 1 : 0.34})`; //0.35
+        let opacityCoin =
+          window.scrollY > positionWallet * 0.958
+            ? 0
+            : window.scrollY > positionMainerBlock * 0.98
+            ? 1
+            : window.scrollY > positonTransactions * 0.987
+            ? 0
+            : 1;
+        coin.style.opacity = `${opacityCoin}`;
+      }
 
-    let transactions = document.querySelector("#transactions");
-    if (
-      window.scrollY > positonTransactions &&
-      window.scrollY < positionCard * 0.984
-    ) {
-      transactions.style.transform = `translate(0, ${
-        (window.pageYOffset - positonTransactions) * 1.3
-      }px) scale(${window.scrollY <= positonTransactions * 1.005 ? 1 : 0.6})`; //0.35
-    }
+      let transactions = document.querySelector("#transactions");
+      if (
+        window.scrollY > positonTransactions &&
+        window.scrollY < positionCard * 0.984
+      ) {
+        transactions.style.transform = `translate(0, ${
+          (window.pageYOffset - positonTransactions) * 1.3
+        }px) scale(${window.scrollY <= positonTransactions * 1.005 ? 1 : 0.6})`; //0.35
+        transactions.style.opacity = `${
+          window.scrollY > positionCard * 0.98 ? 0 : 1
+        }`;
+      }
 
-    let card = document.querySelector("#card");
-    if (
-      window.scrollY > positionCard &&
-      window.scrollY < positionCardBlock * 0.974
-    ) {
-      card.style.transform = `translate(0, ${
-        (window.pageYOffset - positionCard) * 1.3
-      }px) scale(${window.scrollY <= positionCard * 1.012 ? 1 : 0.38})`; //0.35
+      let card = document.querySelector("#card");
+      if (
+        window.scrollY > positionCard &&
+        window.scrollY < positionCardBlock * 0.9658
+      ) {
+        card.style.transform = `translate(0, ${
+          (window.pageYOffset - positionCard) * 1.3
+        }px) scale(${window.scrollY <= positionCard * 1.012 ? 1 : 0.38})`; //0.35
+        card.style.opacity = `${
+          window.scrollY > positionCardBlock * 0.964 ? 0 : 1
+        }`;
+      }
+
+      let cardBlock = document.querySelector("#cardBlock");
+      if (
+        window.scrollY > positionCardBlock &&
+        window.scrollY < positionGroupCardBlock * 0.974
+      ) {
+        cardBlock.style.transform = `translate(0, ${
+          (window.pageYOffset - positionCardBlock) * 1.3
+        }px) scale(${window.scrollY <= positionCardBlock * 1.012 ? 1 : 0.26})`;
+        cardBlock.style.opacity = `${
+          window.scrollY > positionGroupCardBlock * 0.973 ? 0 : 1
+        }`;
+      }
+
+      let groupCardBlock = document.querySelector("#groupCardBlock");
+      if (
+        window.scrollY > positionGroupCardBlock &&
+        window.scrollY < positionMainerBlock * 0.982
+      ) {
+        groupCardBlock.style.transform = `translate(0, ${
+          (window.pageYOffset - positionGroupCardBlock) * 1.3
+        }px)`;
+        groupCardBlock.style.opacity = `${
+          window.scrollY > positionMainerBlock * 0.98 ? 0 : 1
+        }`;
+      }
+
+      let wallet = document.querySelector("#wallet");
+      if (
+        window.scrollY > positionWallet &&
+        window.scrollY < positionLock * 0.974
+      ) {
+        wallet.style.transform = `translate(0, ${
+          (window.pageYOffset - positionWallet) * 1.3
+        }px)`;
+      }
     }
   };
+
   useEffect(() => {
-    let coin = document.querySelector("#mainBitcoin");
-    setPositionCoin(window.pageYOffset + coin.getBoundingClientRect().top);
-
-    let transactions = document.querySelector("#transactions");
-    setPositionTransactions(
-      window.pageYOffset + transactions.getBoundingClientRect().top
-    );
-
-    let card = document.querySelector("#card");
-    setPositionCard(window.pageYOffset + card.getBoundingClientRect().top);
-
-    let cardBlock = document.querySelector("#cardBlock");
-    setPositionCardBlock(
-      window.pageYOffset + cardBlock.getBoundingClientRect().top
-    );
+    getPositionElements();
   }, []);
-
+  useEffect(() => {
+    window.addEventListener("resize", getPositionElements);
+    return () => {
+      window.removeEventListener("resize", getPositionElements);
+    };
+  });
   useEffect(() => {
     window.addEventListener("scroll", listener);
     return () => {
       window.removeEventListener("scroll", listener);
     };
   });
+  const getPositionElements = () => {
+    setTimeout(() => {
+      let coin = document.querySelector("#mainBitcoin");
+      setPositionCoin(getCoords(coin).top);
+      let transactions = document.querySelector("#transactions");
+      setPositionTransactions(getCoords(transactions).top);
+
+      let card = document.querySelector("#card");
+      setPositionCard(getCoords(card).top);
+
+      let cardBlock = document.querySelector("#cardBlock");
+      setPositionCardBlock(getCoords(cardBlock).top);
+
+      let groupCardBlock = document.querySelector("#groupCardBlock");
+      setPositionGroupCardBlock(getCoords(groupCardBlock).top);
+
+      let mainerBlock = document.querySelector("#mainerBlock");
+      setPositionMainerBlock(getCoords(mainerBlock).top);
+
+      let wallet = document.querySelector("#wallet");
+      setPositionWallet(getCoords(wallet).top);
+
+      let lock = document.querySelector("#lock");
+      setPositionLock(getCoords(lock).top);
+    }, 300);
+  };
+  const getCoords = (elem) => {
+    const box = elem.getBoundingClientRect(),
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {
+      top: box.top + scrollTop,
+    };
+  };
   return (
     <div className="containerTechnicalSide">
       <div className="titleBlock">
@@ -114,7 +195,7 @@ export default function TechnicalSide() {
             <img
               src="/images/technicalSide/bitcoin.svg"
               id="mainBitcoin"
-              style={{ transition: "all 0.5s ease", zIndex: 1 }}
+              style={{ transition: "all 0.3s ease", zIndex: 1 }}
             />
           </div>
           <div>
@@ -132,7 +213,7 @@ export default function TechnicalSide() {
             <img
               src="/images/technicalSide/transactions.svg"
               id="transactions"
-              style={{ transition: "all 0.5s ease", zIndex: 2 }}
+              style={{ transition: "all 0.3s ease", zIndex: 2 }}
             />
           </div>
           <div>
@@ -160,7 +241,7 @@ export default function TechnicalSide() {
             <img
               src="/images/technicalSide/account.svg"
               id="card"
-              style={{ transition: "all 0.5s ease", zIndex: 3 }}
+              style={{ transition: "all 0.3s ease", zIndex: 3 }}
             />
           </div>
           <div>
@@ -201,7 +282,7 @@ export default function TechnicalSide() {
             <img
               src="/images/technicalSide/block.svg"
               id="cardBlock"
-              style={{ transition: "all 0.5s ease", zIndex: 3 }}
+              style={{ transition: "all 0.3s ease", zIndex: 3 }}
             />
           </div>
           <div>
@@ -209,9 +290,197 @@ export default function TechnicalSide() {
             <div>
               Блок – это запись в распределенном реестре о нескольких
               транзакциях. В нем отражено, кто кому и когда перевел какой объём
-              активов. Все блоки последовательно соединяются в одну цепь.
+              активов. Все блоки последовательно соединяются в одну цепь, так
+              как каждый блок содержит свой собственный уникальный
+              криптографический идентификатор, который связывает его с
+              предыдущим блоком цепи.
             </div>
           </div>
+        </div>
+        <div className="infoBlockchain">
+          <div>
+            <img
+              src="/images/technicalSide/groupChain.svg"
+              id="groupCardBlock"
+              style={{ transition: "all 0.3s ease", zIndex: 3 }}
+            />
+          </div>
+          <div>
+            <div className="titleBlock">Цепь</div>
+            <div>
+              Цепь блокчейна неразрывна, поскольку каждый блок содержит
+              уникальный идентификатор, который связывает его с предыдущим.
+              Блоки нельзя изменить или удалить, можно только добавить новые.
+              После добавления в блокчейн, блоки уже невозможно изменить без
+              потери данных о всей последующей цепи, что незамедлительно дает
+              другим пользователям знать, что было совершено стороннее
+              вмешательство в обход правил. Это даёт возможность просто отказать
+              в использовании изменённой версии цепочки (т. к. без признания
+              изменённого блока со стороны большинства участников процесса он
+              бесполезен) и продолжать работать с исходной ветвью.
+            </div>
+            <div>
+              Таким образом, всегда можно восстановить историю переходов
+              конкретного биткойна из рук в руки и узнать его нынешнего
+              владельца. Новые блоки в цепь добавляют майнеры.
+            </div>
+          </div>
+        </div>
+        <div className="infoBlockchain">
+          <div>
+            <img
+              src="/images/technicalSide/mainer.svg"
+              id="mainerBlock"
+              style={{ transition: "all 0.3s ease", zIndex: 3 }}
+            />
+          </div>
+          <div>
+            <div className="titleBlock">Майнеры</div>
+            <div>
+              Чтобы достичь консенсуса относительно того, какие блоки транзакций
+              действительно должны быть добавлены в цепочку блоков и для того,
+              чтобы банально создавать эти блоки данных, некоторые пользователи
+              участвуют в процессе{" "}
+              <a href="https://ru-bitcoinwiki-org.turbopages.org/ru.bitcoinwiki.org/s/wiki/%D0%9C%D0%B0%D0%B9%D0%BD%D0%B8%D0%BD%D0%B3">
+                майнинга
+              </a>{" "}
+              (с англ. mining — добыча, горное дело). Замечание: для того, чтобы
+              пользоваться криптовалютой, её не обязательно «добывать».
+            </div>
+            <div>
+              Майнеры с помощью специализированного оборудования и программного
+              обеспечения выполняют сложные математические вычисления, в
+              результате которых создаются новые блоки в блокчейне. За создание
+              очередной структурной единицы обычно предусмотрено вознаграждение
+              за счёт новых единиц криптовалюты и/или комиссионных сборов.
+            </div>
+            <div>
+              Майнеры выполняют в блокчейне несколько функций:
+              <ul>
+                <li>
+                  хранят копии блокчейна и тем самым защищают информацию от
+                  потери или подделки;
+                </li>
+                <li>подтверждают транзакции;</li>
+                <li>
+                  проверяют транзакции, которые зарегистрировали другие майнеры.
+                  Чем их больше, тем лучше – такая сеть надежнее; майнерами
+                  могут стать все желающие.
+                </li>
+              </ul>
+            </div>
+            <div>
+              Майнинг — не единственная технология создания новых блоков и
+              обеспечения эмиссии. Альтернативами являются{" "}
+              <a href="https://cryptocurrency.tech/forzhing-kak-perspektivnaya-alternativa-majningu/">
+                форжинг (минтинг)
+              </a>{" "}
+              и{" "}
+              <a href="https://www.figma.com/exit?url=https%3A%2F%2Fcryptocurrency.tech%2Fforzhing-kak-perspektivnaya-alternativa-majningu%2F">
+                ICO
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+        <div className="infoBlockchain">
+          <div>
+            <img
+              src="/images/technicalSide/wallet.svg"
+              id="wallet"
+              style={{ transition: "all 0.3s ease", zIndex: 3 }}
+            />
+          </div>
+          <div>
+            <div className="titleBlock">Кошелек</div>
+            <div>
+              Пользователи криптовалют используют специальные программные или
+              аппаратные кошельки. Каждый такой кошелек содержит в себе
+              уникальный криптографический ключ, который позволяет владельцу
+              получить доступ к своим накоплениям, которые хранятся в рамках
+              общедоступного блокчейна. Создатели криптовалют придумали
+              интересную схему безопасности — для каждой операции кошелек
+              создает новые уникальные адреса.
+            </div>
+            <div>
+              При этом кошельки могут быть как «горячими» — то есть размещаться
+              где-то в интернете в рамках онлайн-сервиса, так и «холодными» —
+              хранение криптовалюты происходит без доступа к сети. «Холодный»
+              кошелек, фактически, является файлом на компьютере, потеря
+              которого обернется безвозвратной потерей доступа к кошельку и
+              криптовалюте внутри него.
+            </div>
+            <div>
+              Чаще всего такие кошельки анонимны – они не позволяют узнать, кто
+              именно принимает или отправляет с него активы. Но если владелец
+              кошелька, например, забудет его номер, то никак не сможет
+              доказать, что счет принадлежит именно ему. Все, что хранилось в
+              кошельке, окажется потерянным навсегда. Данные электронных
+              кошельков и транзакции в блокчейне защищены шифрованием.
+            </div>
+          </div>
+        </div>
+        <div className="infoBlockchain">
+          <div className="lockBlock">
+            <img
+              src="/images/technicalSide/lock.svg"
+              id="lock"
+              style={{
+                transition: "all 0.3s ease",
+                zIndex: 4,
+                width: "6vw",
+                marginLeft: "11vw",
+              }}
+            />
+          </div>
+          <div>
+            <div className="titleBlock">Шифрование</div>
+            <div>
+              Благодаря криптографии, которая лежит в основе криптовалют, и
+              вытекающих из неё методов шифрования, информация о транзакциях и
+              состоянии кошелька остаётся верной, полной и конфиденциальной.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        В блокчейн-сетях покупатель и продавец биткойна подтверждают транзакцию
+        с помощью криптографических ключей – специальных уникальных цифровых
+        кодов. Угадать последовательность символов цифрового кода
+        криптографических ключей практически невозможно. Это делает технологию
+        блокчейн одной из лучших для финансовых транзакций. Но в то же время уже
+        были случаи взлома кошельков, поэтому их лучше подключать к сети только
+        на время проведения транзакций, а в остальное время пользоваться
+        «холодным» кошельком.
+      </div>
+      <div>
+        Если вы хотите узнать больше о технической стророне криптовалют,
+        предлагаем вам посмотреть следующие видео:
+      </div>
+      <div className="videoCoinBlock">
+        <div className="videoContent">
+          <div>
+            <iframe
+              src="https://www.youtube.com/embed/00CJAVkBlJE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div>«Как работает биткойн?» — видео канала «ТОПЛЕС», YouTube.</div>
+        </div>
+        <div className="videoContent">
+          <div>
+            <iframe
+              src="https://www.youtube.com/embed/G2d3cz84sLM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div>«Блокчейн: определения, приложения, задачи» — лекция Юрия Яновича, НИУ ВШЭ.</div>
         </div>
       </div>
     </div>
