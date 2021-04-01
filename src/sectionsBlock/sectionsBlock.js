d02d4912b180401a5aefe388e7cf302a27cd9a38
Данил <abanin-02@mail.ru>
2021-04-02 01:00:49 +0400

    added 2 blocks

--- src/sectionsBlock/sectionsBlock.js
+++ src/sectionsBlock/sectionsBlock.js
@@ -5,6 +5,8 @@ import './sectionsBlock.scss'
 import History from './sections/history/history'
 import TechnicalSide from './sections/technicalSide/technicalSide'
 import UseCoin from './sections/useCoin/useCoin'
+import ValutionCoin from './sections/valuationCoin/valutionCoin'
+import Questions from './sections/questions/questions'
 export default function SectionsBlock() {
     return (
         <div className="containerSections">
@@ -13,6 +15,8 @@ export default function SectionsBlock() {
             <History/>
             <TechnicalSide/>
             <UseCoin/>
+            <ValutionCoin/>
+            <Questions/>
         </div>
     )
 }
